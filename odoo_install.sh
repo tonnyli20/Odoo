eb9026b0566d146b8fc5f246f85953ccac3a11d5
tonnyli20 <62580875+tonnyli20@users.noreply.github.com>
2024-07-31 07:57:55 +0800

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -72,8 +72,11 @@ sudo add-apt-repository universe
 sudo add-apt-repository "deb http://mirrors.kernel.org/ubuntu/ xenial main"
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install libpq-dev
-
+sudo apt install git python3-pip python3-dev libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev gdebi-core python3-wheel python3-venv libxslt-dev libzip-dev -y
+sudo apt-get install libpq-dev npm -y
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install -g less less-plugin-clean-css
+sudo apt-get install -y node-less
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
